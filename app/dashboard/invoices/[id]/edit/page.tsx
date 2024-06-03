ebd9326a837304a161aab884b6022d1c735a5af8
sinetmd <dobremrn12@gmail.com>
2024-06-03 15:56:34 +0300

    refactoring small import error for font.ts file

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,34 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/edit-form';
+import React from 'react';
+import NotFound from './not-found';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    NotFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+import { URL } from 'url';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, build with App Router',
+  metadataBase: new URL('https://new-url.com'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
